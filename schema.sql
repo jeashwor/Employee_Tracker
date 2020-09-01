7670d1b8d19733ae3e5a493be934a1e5ea16eb42
Jacob Ashworth <jeashwor@gmail.com>
2020-08-31 22:10:00 -0400

    wrote initial schema.sql file to create database as described in readme file

---
+++ schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employeeTrackerDB;
+CREATE DATABASE employeeTrackerDB;
+
+USE employeeTrackerDB;
+
+CREATE TABLE department(
+    id INT NOT NULL,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role(
+    id INT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL(10,2),
+    department_id INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee(
+    id INT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+)
+
