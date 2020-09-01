9bf05812d1b326826d1ba4952fded7456fe8a188
Jacob Ashworth <jeashwor@gmail.com>
2020-08-31 23:21:26 -0400

    updated schema file with Foriegn Key attributes

--- schema.sql
+++ schema.sql
@@ -14,7 +14,8 @@ CREATE TABLE role(
     title VARCHAR(30),
     salary DECIMAL(10,2),
     department_id INT,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(name)
 );
 
 CREATE TABLE employee(
@@ -23,6 +24,8 @@ CREATE TABLE employee(
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(title),
+    FOREIGN KEY (manager_id) REFERENCES role(department_id)
 );
 
